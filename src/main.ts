a2c34562a2f10ffd2d78427d6e80e583ec8f30bf
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-03-07 16:12:07 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,8 @@ app */
   declared above*/
   nunjucks.configure(views, { express });
   //start the application
+  const staticAssets = join(__dirname, '..', 'static');
+  app.useStaticAssets(staticAssets);
   await app.listen(3000);
 }
 bootstrap();
