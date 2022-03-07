101a2dc5a685bac7e9d608d357f9fe22b3303bbb
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-03-07 16:51:26 +0100

    update

--- src/farminventory/inventories/inventories.module.ts
+++ src/farminventory/inventories/inventories.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { InventoriesService } from './inventories.service';
 import { InventoriesController } from './inventories.controller';
-
+import { FarminventoryModule } from '../farminventory.module';
 @Module({
+  imports: [TypeOrmModule.forRoot(), FarminventoryModule],
   controllers: [InventoriesController],
   providers: [InventoriesService]
 })
