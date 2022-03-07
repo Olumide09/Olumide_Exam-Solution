20d1c96a246dcfa7b5d4294e93eabf373d14cdfe
Olumide09 <92790819+Olumide09@users.noreply.github.com>
2022-03-07 16:32:58 +0100

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FarminventoryModule } from './farminventory/farminventory.module';
 
 @Module({
-  imports: [],
+  imports: [FarminventoryModule],
   controllers: [AppController],
   providers: [AppService],
 })
